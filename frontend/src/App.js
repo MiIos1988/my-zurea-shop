43b63f2899d504b859edec517c0506c14c71a368
Milos1988 <vojvoda1988>
2023-02-16 01:34:48 +0100

    Show and hide password works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import LoginSectionComponent from "./components/loginSection/LoginSectionComponent";
 
 axios.defaults.baseURL = "http://localhost:5050/api";
 
 function App() {
   return (
     <>
-      
+      <LoginSectionComponent/>
     </>
   );
 }
