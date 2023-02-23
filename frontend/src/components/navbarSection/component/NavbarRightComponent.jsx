989b1cf1970b2430189db831d0a6cfe41dadaaf3
Milos1988 <vojvoda1988@gmail.com>
2023-02-23 11:52:55 +0100

    Search element

--- frontend/src/components/navbarSection/component/NavbarRightComponent.jsx
+++ frontend/src/components/navbarSection/component/NavbarRightComponent.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import DropdownUserComponent from '../dropdowns/dropdownUser/DropdownUserComponent'
 import { useState } from "react"
+import SearchElementComponent from './SearchElementComponent'
 
 const NavbarRightComponent = () => {
   const [open, setOpen] = useState(false)
   return (
     <div className="navRight col-3 d-flex justify-content-end">
       <div className="searchWrap d-flex align-items-center me-1">
-        <div className="search"></div>
+        <SearchElementComponent />
         <DropdownUserComponent open={open} />
       </div>
 
